a6f080cac6362d22c17d3f06b214ea41ebd0e71e
codestellations <erlindaargyt@gmail.com>
2021-01-14 06:45:41 +0700

    Dashboard 1.1
    - ganti bahasa jadi Indonesia
    - menambahkan modal (overlay) Tambah Tugas dan Edit Tugas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
-<style>
-        body {
-        height: 100%;
-        }
+    <style>
+            body {
+            height: 100%;
+            }
 
-        .vertical-center {
-        min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
-        min-height: 100vh; /* These two lines are counted as one :-)       */
-        width: 100%;
-        display: flex;
-        align-items: center;
-        }
-</style>
+            .vertical-center {
+            min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+            min-height: 100vh; /* These two lines are counted as one :-)       */
+            width: 100%;
+            display: flex;
+            align-items: center;
+            }
+    </style>
+
+
 
     @if(session('success'))
         <div class="alert alert-success" role="alert">
@@ -25,20 +27,14 @@
             {{session('fail')}}
         </div>
     @endif
+    <head>
+        <title>Dashboard</title>
+    </head>
 
-    {{-- <div class="row text-center vertical-center">
-        <div class="col">
-            <h1 class="mb-2 display-4">Welcome, {{auth()->user()->name}}!</h1>
-            <blockquote class="blockquote">
-            <p class="mb-2">Thought is the wind, knowledge the sail, and mankind the vessel.</p>
-            <footer class="blockquote-footer"><cite title="Source Title">Augustus Hare</cite></footer>
-            </blockquote>
-        </div>
-    </div> --}}
-
-    <div class="container">
-        <div class="row justify-content-md-center text-center vertical-center">
-            {{-- <div class="container"> --}}
+    <body>
+        <div class="container">
+            {{-- bagian kiri --}}
+            <div class="row justify-content-md-center text-center vertical-center">
                 <div class="container col-md-auto">
                     <div class="pricing-header px-3 py-3 pt-md-3 pb-md-3 mx-auto text-center">
                         <div class="px-3 py-3 pt-md-3 pb-md-3">
@@ -48,8 +44,8 @@
                         
                         <h1 class="display-4">25 : 00 : 00</h1>
 
-                        <button type="button" class="btn btn-primary m-3" data-toggle="button" aria-pressed="false" autocomplete="off">
-                            Start
+                        <button type="button" class="btn btn-primary m-3 btn-lg" data-toggle="button" aria-pressed="false" autocomplete="off">
+                            Mulai
                         </button>
 
                         <div>
@@ -58,66 +54,103 @@
                                 <input type="radio" name="options" id="option1" autocomplete="off" checked> Pomodoro
                             </label>
                             <label class="btn btn-secondary">
-                                <input type="radio" name="options" id="option2" autocomplete="off"> Short Break
+                                <input type="radio" name="options" id="option2" autocomplete="off"> Istirahat Sejenak
                             </label>
                             <label class="btn btn-secondary">
-                                <input type="radio" name="options" id="option3" autocomplete="off"> Long Break
+                                <input type="radio" name="options" id="option3" autocomplete="off"> Istirahat Panjang
                             </label>
                             </div>
                         </div>
                     </div>
                 </div>
 
+                {{-- bagian kanan --}}
                 <div class="container col-md-auto">
-                    <h1 class="px-3 py-3 pt-md-5 pb-md-4 display-4">Hello, Steve.</i></h1>
+                    <h1 class="px-3 py-3 pt-md-5 pb-md-4 display-4">Halo, Steve.</i></h1>
                     {{-- <h1 class="mb-2 display-4">Welcome, {{auth()->user()->name}}!</h1> --}}
                 <div class="card-deck mb-3 text-center">
                         <div class="card mb-4 box-shadow">
                         <div class="card-header">
-                            <h4 class="my-0 font-weight-normal">Tasks</h4>
+                            <h4 class="my-0 font-weight-normal">Daftar Tugas</h4>
                         </div>
                         <div class="card-body">                            
                             <ul class="list-group mt-3 mb-4 text-left">
-                                <li class="list-group-item d-flex justify-content-between">Learning Grafkom
-                                    <span class="iconify align-self-center" data-icon="bi-three-dots" data-inline="false"></span>
+                                <li class="list-group-item d-flex justify-content-between">Selesaikan Grafkom
+                                    <span type="button" class="iconify align-self-center" data-icon="bi-three-dots" data-inline="false"
+                                        data-toggle="modal" data-target="#editTugas"></span>
                                 </li>
-                                <li class="list-group-item d-flex justify-content-between disabled">Finishing Mockup
+                                <li class="list-group-item d-flex justify-content-between disabled">Perbaiki Mockup
                                     <span class="iconify align-self-center" data-icon="bi-three-dots" data-inline="false"></span>
                                 </li>
                             </ul>
-                            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Add task</button>
+                            {{-- button trigger modal --}}
+                            <button type="button" class="btn btn-lg btn-block btn-outline-primary" data-toggle="modal" 
+                                    data-target="#tambahTugas">Tambah Tugas</button>
                         </div>
                         </div>
 
                     </div>
                 </div>
-            {{-- </div> --}}
-
-
-            {{-- <div class="container">
-                <div class="card-deck mb-3 text-center">
-                    <div class="card mb-4 box-shadow">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">Free</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                        <li>10 users included</li>
-                        <li>2 GB of storage</li>
-                        <li>Email support</li>
-                        <li>Help center access</li>
-                        </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-                    </div>
-                    </div>
-
-                </div>
-            </div> --}}
+            </div>
         </div>
 
-    </div>
+        <!-- Modal tambah tugas-->
+        <div class="modal fade" id="tambahTugas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tugasBaru">Tugas Baru</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="taskName" class="form-label">Nama tugas</label>
+                    <input type="text" class="form-control" id="taskName" placeholder="Contoh : Memasak telur">
+                </div>
+                <div class="mb-3">
+                    <label for="taskInterval" class="form-label">Interval</label>
+                    <input type="text" class="form-control" id="taskInterval" placeholder="Contoh : 1">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button class="btn btn-primary" type="submit" value="Submit">Simpan</button>
+            </div>
+            </div>
+        </div>
+        </div>
 
+        
+        <!-- Modal edit tugas-->
+        <div class="modal fade" id="editTugas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tugasBaru">Edit Tugas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="taskName" class="form-label">Nama tugas</label>
+                    <input type="text" class="form-control" id="taskName" placeholder="Memasak telur">
+                </div>
+                <div class="mb-3">
+                    <label for="taskInterval" class="form-label">Interval</label>
+                    <input type="text" class="form-control" id="taskInterval" placeholder="1">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button class="btn btn-primary" type="submit" value="Submit">Simpan</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </body>
 @endsection
     
 
