4b61cc75be44d6c9164c2e9bf1ddf57e92ac3edd
codestellations <erlindaargyt@gmail.com>
2021-01-14 15:52:02 +0700

    Dashboard 1.2
    - fixed layout (side by side)
    - added HapusTask modal
    - separated dashboard to index, edit, create

---
+++ resources/views/dashboard/create.blade.php
@@ -0,0 +1,27 @@
+        <!-- Modal tambah tugas-->
+        <div class="modal fade" id="tambahTugas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tugasBaru">Tugas Baru</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="taskName" class="form-label">Nama tugas</label>
+                    <input type="text" class="form-control" id="taskName" placeholder="Contoh : Memasak telur">
+                </div>
+                <div class="mb-3">
+                    <label for="taskInterval" class="form-label">Interval</label>
+                    <input type="text" class="form-control" id="taskInterval" placeholder="Contoh : 1">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
+                <button class="btn btn-primary" type="submit" value="Submit">Simpan</button>
+            </div>
+            </div>
+        </div>
+        </div>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -27,7 +27,10 @@
     <head>
         <title>Dashboard</title>
     </head>
-    
+    <body>
+        @include('dashboard.create')
+        @include('dashboard.edit')
+
         <div class="container">
             {{-- bagian kiri --}}
             <div class="row text-center vertical-center">
@@ -105,64 +108,7 @@
             </div>
         </div>
 
-        <!-- Modal tambah tugas-->
-        <div class="modal fade" id="tambahTugas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="tugasBaru">Tugas Baru</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label for="taskName" class="form-label">Nama tugas</label>
-                    <input type="text" class="form-control" id="taskName" placeholder="Contoh : Memasak telur">
-                </div>
-                <div class="mb-3">
-                    <label for="taskInterval" class="form-label">Interval</label>
-                    <input type="text" class="form-control" id="taskInterval" placeholder="Contoh : 1">
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
-                <button class="btn btn-primary" type="submit" value="Submit">Simpan</button>
-            </div>
-            </div>
-        </div>
-        </div>
-
-        
-        <!-- Modal edit tugas-->
-        <div class="modal fade" id="editTugas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="tugasBaru">Edit Tugas</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label for="taskName" class="form-label">Nama tugas</label>
-                    <input type="text" class="form-control" id="taskName" placeholder="Memasak telur">
-                </div>
-                <div class="mb-3">
-                    <label for="taskInterval" class="form-label">Interval</label>
-                    <input type="text" class="form-control" id="taskInterval" placeholder="1">
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
-                <button class="btn btn-success" type="submit" value="Submit">Simpan</button>
-            </div>
-            </div>
-        </div>
-        </div>
-
-        {{-- hapus tugas modal --}}
+        {{-- Modal hapus tugas --}}
         <div class="modal fade" id="hapusTugas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
@@ -183,7 +129,7 @@
         </div>
         </div>
         {{-- <script type="module" src="{{ asset('js/main.js') }}"></script> --}}
-    
+    </body>
 @endsection
 @section('scripts')
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/101/three.min.js"></script> --}}
